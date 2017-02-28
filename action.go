fce605f02a83ceb8005cc58151bb47d8e5a79ebb
Julian Picht <julian.picht@gmail.com>
2017-02-28 21:22:15 +0100

    data should be "raw"

--- action.go
+++ action.go
@@ -2,23 +2,21 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"github.com/jpicht/logger"
 	"io"
 	"net/http"
 )
 
 type action struct {
-	Action   string      `yaml:"action"`
-	URL      string      `yaml:"url"`
-	Method   string      `yaml:"method"`
-	Expected []int       `yaml:"expected"`
-	Data     interface{} `yaml:"data"`
+	Action   string `yaml:"action"`
+	URL      string `yaml:"url"`
+	Method   string `yaml:"method"`
+	Expected []int  `yaml:"expected"`
+	Data     string `yaml:"data"`
 }
 
 func (a *action) Run(r *recipe, l logger.Logger) bool {
-	tmp, _ := json.Marshal(a.Data)
-	req, err := http.NewRequest(a.Method, r.url(a.URL), bytes.NewBuffer(tmp))
+	req, err := http.NewRequest(a.Method, r.url(a.URL), bytes.NewBufferString(a.Data))
 	if err != nil {
 		l.Errorf("Creating request failed: '%s'", err)
 		return false
